e0785b4439ca832add11af20d4f92bdbff58a389
Cihad Horuzoglu <cihadhoruzoglu@gmail.com>
2014-11-19 23:24:49 +0200

    Html files added

---
+++ app/html/templates.js
@@ -0,0 +1,65 @@
+(function(module) {
+try {
+  module = angular.module('templates');
+} catch (e) {
+  module = angular.module('templates', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('partials/home.html',
+    '<ion-view title="Home">\n' +
+    '    <ion-nav-buttons side="left">\n' +
+    '        <button menu-toggle="left"class="button button-icon icon ion-navicon"></button>\n' +
+    '    </ion-nav-buttons>\n' +
+    '    <ion-content class="has-header">\n' +
+    '        <h1>Home Page</h1>\n' +
+    '        <ion-item ng-repeat="venue in venues.data.response.venues">\n' +
+    '            {{venue.name}}\n' +
+    '        </ion-item>\n' +
+    '    </ion-content>\n' +
+    '</ion-view>\n' +
+    '');
+}]);
+})();
+
+(function(module) {
+try {
+  module = angular.module('templates');
+} catch (e) {
+  module = angular.module('templates', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('partials/menu.html',
+    '<ion-side-menus>\n' +
+    '\n' +
+    '    <ion-pane ion-side-menu-content>\n' +
+    '        <ion-nav-bar class="bar-stable nav-title-slide-ios7">\n' +
+    '            <ion-nav-back-button class="button-clear"><i class="icon ion-ios7-arrow-back"></i> Back</ion-nav-back-button>\n' +
+    '        </ion-nav-bar>\n' +
+    '        <ion-nav-view name="menuContent" animation="slide-left-right"></ion-nav-view>\n' +
+    '    </ion-pane>\n' +
+    '\n' +
+    '    <ion-side-menu side="left">\n' +
+    '        <header class="bar bar-header bar-stable">\n' +
+    '            <h1 class="title">Left</h1>\n' +
+    '        </header>\n' +
+    '        <ion-content class="has-header">\n' +
+    '            <ion-list>\n' +
+    '                <ion-item nav-clear menu-close ng-click="login()">\n' +
+    '                    Login\n' +
+    '                </ion-item>\n' +
+    '                <ion-item nav-clear menu-close href="#/app/search">\n' +
+    '                    Search\n' +
+    '                </ion-item>\n' +
+    '                <ion-item nav-clear menu-close href="#/app/browse">\n' +
+    '                    Browse\n' +
+    '                </ion-item>\n' +
+    '                <ion-item nav-clear menu-close href="#/app/playlists">\n' +
+    '                    Playlists\n' +
+    '                </ion-item>\n' +
+    '            </ion-list>\n' +
+    '        </ion-content>\n' +
+    '    </ion-side-menu>\n' +
+    '</ion-side-menus>\n' +
+    '');
+}]);
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -9,35 +9,7 @@ var gulp        = require('gulp'),
     iconfont    = require('gulp-iconfont'),
     gulpDocs    = require('gulp-ngdocs');
 
-var bases = {
-    app: 'app/',
-    dist: 'dist/'
-};
-
-var paths = {
-    libs: [
-        'app/lib/ionic/release/js/ionic.min.js',
-        'app/lib/angular/angular.min.js',
-        'app/lib/angular-animate/angular-animate.min.js',
-        'app/lib/angular-sanitize/angular-sanitize.min.js',
-        'app/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'app/lib/ionic/release/js/ionic-angular.min.js'
-    ],
-    js: ['js/**/*.js', '!js/lib/']
-
-};
-
 gulp.task('default', ['js', 'css', 'ngHtml2js', 'connect', 'watch']);
-
-gulp.task('ngdocs', [], function () {
-//    var options = {
-//        scripts: ['../app.min.js']
-//    };
-
-    return gulp.src(['./app/controllers/*.js', './app/app.js'])
-        .pipe(gulpDocs.process())
-        .pipe(gulp.dest('docs'));
-});
 
 gulp.task('js', function() {
    gulp.src(['./app/js/**/*.js', '!./app/js/app.js', '!./app/js/app.min.js'])
@@ -48,25 +20,24 @@ gulp.task('js', function() {
        .pipe(gulp.dest('./app/js/'))
 });
 
-gulp.task('libjs', function() {
-    gulp.src(paths.libs)
-        .pipe(concat('lib.js'))
-        .pipe(gulp.dest('./public/js/'))
+gulp.task('css', function () {
+    gulp.src(['./app/css/*.less'])
+        .pipe(less())
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('./app/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./app/css'))
 });
 
-gulp.task('font', function() {
-   gulp.src('app/lib/ionic/release/fonts/*.*')
-       .pipe(gulp.dest('./public/fonts'))
-});
-
-gulp.task('ngHtml2js', function () {
+gulp.task('html', function () {
     gulp.src('./app/html/*.html')
         .pipe(ngHtml2Js({
             moduleName: 'templates',
             prefix: 'partials/'
         }))
-        .pipe(concat('ngtemplate.js'))
-        .pipe(gulp.dest('./public/html'))
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest('./app/html'))
 });
 
 gulp.task('watch', function() {
@@ -83,33 +54,19 @@ gulp.task('watch', function() {
     gulp.watch(['./app/html/*.html'], ['ngHtml2js']);
 });
 
+gulp.task('ngdocs', [], function () {
+//    var options = {
+//        scripts: ['../app.min.js']
+//    };
+
+    return gulp.src(['./app/controllers/*.js', './app/app.js'])
+        .pipe(gulpDocs.process())
+        .pipe(gulp.dest('docs'));
+});
+
 gulp.task('connect', function() {
     connect.server({
         livereload: true
     });
 });
-
-gulp.task('css', function () {
-    gulp.src(['./app/css/*.less'])
-        .pipe(less())
-        .pipe(concat('app.css'))
-        .pipe(gulp.dest('./app/css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(gulp.dest('./app/css'))
-});
-
-gulp.task('iconfont', function(){
-    gulp.src(['app/icons/*.svg'])
-        .pipe(iconfont({
-            fontName: 'myfont', // required
-            appendCodepoints: true // recommended option
-        }))
-        .on('codepoints', function(codepoints, options) {
-            // CSS templating, e.g.
-            console.log(codepoints, options);
-        })
-        .pipe(gulp.dest('public/fonts/'));
-});
-
 
